de9f729ee5c1be99d619e18ff07a2bf1be818643
Ricky Riskiawan <rickyrizkiawan666@gmail.com>
2023-06-07 18:21:45 +0700

    adding function passing data

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,18 +1,27 @@
 import InputForm from "./InputForm";
 import Button from "../component/Button";
+import { useState } from "react";
 
 export default function Form() {
-  function dataInput(data) {
-    console.log(data);
-  }
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [date, setDate] = useState("");
+
+  const titleInput = (data) => setTitle(data);
+  const priceInput = (data) => setPrice(data);
+  const dateInput = (data) => setDate(data);
+
+  console.log(`dari title ${title}`);
+  console.log(`dari price ${price}`);
+  console.log(`dari date ${date}`);
 
   return (
     <div className="bg-slate-900 p-4 container max-w-[95%] w-[50rem] m-auto rounded-2xl">
       <form action="" className="px-5 py-4 bg-slate-800 rounded-2xl">
         <div className="mb-8">
-          <InputForm title="Title" typeinput="text" userInput={dataInput} />
-          <InputForm title="Price" typeinput="number" />
-          <InputForm title="Date" typeinput="date" />
+          <InputForm title="Title" typeinput="text" userInput={titleInput} />
+          <InputForm title="Price" typeinput="number" userInput={priceInput} />
+          <InputForm title="Date" typeinput="date" userInput={dateInput} />
         </div>
 
         <div className="flex justify-end mb-3">
--- src/form/InputForm.jsx
+++ src/form/InputForm.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-
 export default function InputForm({ title, typeinput, userInput }) {
-  const [input, setInput] = useState("");
-
   function inputHandler(e) {
-    setInput(e.target.value);
+    return userInput(e.target.value);
   }
 
-  console.log(input);
   return (
     <div className="my-3">
       <label htmlFor="" className="block text-gray-200 text-sm font-bold my-3">
